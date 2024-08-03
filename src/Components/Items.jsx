7bbe47eb7f17c1ca680d240fe5f46a95bd24f993
kafi ahmed <kafyahmedfarabi@gmail.com>
2024-08-03 20:00:28 +0530

    some changes

--- src/Components/Items.jsx
+++ src/Components/Items.jsx
@@ -1,23 +1,24 @@
-import React from 'react'
-import SingleItem from './SingleItem'
+import React from "react";
+import SingleItem from "./SingleItem";
 
-const Items = ({newItems, removeItem, editItems}) => {
-
-    console.log(newItems)
-
-    const catchItems = newItems.map(item => (
-        
-        <SingleItem  key={item.id} item={item} removeItem={removeItem} editItems={editItems} />
-    ))
+const Items = ({ newItems, removeItem, editItems }) => {
+  const catchItems = newItems.map((item) => (
+    <SingleItem
+      key={item.id}
+      item={item}
+      removeItem={removeItem}
+      editItems={editItems}
+    />
+  ));
   return (
-    <div className=' grid grid-cols-1 w-full justify-between gap-2 py-6'>
-        {catchItems}
+    <div className=" grid grid-cols-1 w-full justify-between gap-2 py-6">
+      {catchItems}
 
-        <footer>
-            <h3 className=' text-center'>{newItems.length} Items</h3>
-        </footer>
+      <footer>
+        <h3 className=" text-center"> total = {newItems.length} Items</h3>
+      </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Items
+export default Items;
